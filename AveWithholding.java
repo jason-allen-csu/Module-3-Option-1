83d743e294a93ea17d3a4e7ed6d2f7b4458bb95c
jason-allen-csu <jason.allen@csuglobal.edu>
2024-09-01 20:55:13 -0400

    Commit 2 with correction

--- AveWithholding.java
+++ AveWithholding.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
 
 public class AveWithholding {
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		
 		double weeklyIncome;
-		double taxRate = 0.0;
+		double taxRate = 0.3; // Setting base tax rate to maximum value
 		double weeklyWithholding = 0.0;
 		
 		// User input for weekly salary
 		System.out.print("Enter your weekly income: ");
         weeklyIncome = scnr.nextDouble();
         
+        if (weeklyIncome < 0) {
+       		System.out.println("Invalid income ");
+       		return;
+        }
         // Tax rate calculation
         if (weeklyIncome < 500) {
         	taxRate = 0.1;
@@ -19,9 +24,7 @@ public class AveWithholding {
         	taxRate = 0.15;
         } else if (weeklyIncome < 2500) {
         	taxRate = 0.20;
-        } else {
-        	taxRate = 0.3;
-        }
+        } 
         
         // Tax Withholding
         weeklyWithholding = taxRate * weeklyIncome;
@@ -30,5 +33,7 @@ public class AveWithholding {
         System.out.println("Weekly Income: $" + weeklyIncome);
         System.out.println("Tax Rate: " + (taxRate * 100) + "%");
         System.out.println("Weekly Tax Withholding: $" + weeklyWithholding);
+        
+        scnr.close();
 	}
 }
